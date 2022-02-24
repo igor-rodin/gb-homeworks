4fd1a86a880973c723d92de9bd2c10f2a1817743
Igor Rodin <igreen1974@gmail.com>
2022-02-24 23:26:39 +0300

    Done task 4

---
+++ homework_1/task_4/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите первое чиcло: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе чиcло: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье чиcло: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"a = {firstNumber}");
+Console.WriteLine($"b = {secondNumber}");
+Console.WriteLine($"c = {thirdNumber}");
+
+int maxNumber = firstNumber;
+
+if (secondNumber > firstNumber)
+{
+  maxNumber = secondNumber;
+}
+
+if (thirdNumber > firstNumber)
+{
+  maxNumber = thirdNumber;
+}
+
+Console.WriteLine($"Max = {maxNumber}");
