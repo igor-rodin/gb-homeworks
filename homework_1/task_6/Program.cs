f8abb98e3ce9c67e03f7fbc09931568299fb7c41
Igor Rodin <igreen1974@gmail.com>
2022-02-25 00:29:08 +0300

    Done task 6

---
+++ homework_1/task_6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите чиcло: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+  Console.WriteLine($"Число {number} четное");
+}
+else
+{
+  Console.WriteLine($"Число {number} нечетное");
+}
+
+
