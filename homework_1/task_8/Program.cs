d320d1b049715291be07e0ca0ad520e6a524d547
Igor Rodin <igreen1974@gmail.com>
2022-02-25 10:02:57 +0300

    Done task 8

---
+++ homework_1/task_8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите чиcло: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Четные чиcла от 1 до {number}:");
+for (int i = 2; i <= number; i += 2)
+{
+  Console.Write($" {i}");
+}
+Console.WriteLine("");
