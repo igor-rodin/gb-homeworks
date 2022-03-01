0d4abf1b80ebf36927176e34b2f7c8f1e01c410f
Igor Rodin <igreen1974@gmail.com>
2022-03-01 13:23:31 +0300

    Done task 10

---
+++ homework_2/task_10/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+byte GetSecondDigit(int number)
+{
+  return Convert.ToByte((number / 10) % 10);
+}
+
+Console.Write("Введите трехзначное чиcло: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number > 99 && Number < 1000)
+{
+  byte DigitTwo = GetSecondDigit(Number);
+  Console.WriteLine($"Вторая цифра чиcла {Number}: {DigitTwo}");
+}
+else
+{
+  Console.WriteLine("Чиcло должно быть трехзначным!");
+}
