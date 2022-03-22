614771e124a064262923389936a265f5700f98f5
igor-rodin <igreen1974@gmail.com>
2022-03-22 12:14:50 +0300

    Done task 29

--- homework_4/task_27/Program.cs
+++ homework_4/task_27/Program.cs
@@ -1,29 +1,30 @@
 ﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 int SumOfDigits(int Number)
 {
-  int result = 0;
+  int Result = 0;
   int PosNumber = Math.Abs(Number);
   while (PosNumber > 0)
   {
     int Digit = PosNumber % 10;
-    result += Digit;
+    Result += Digit;
     PosNumber /= 10;
   }
-  return result;
+  return Result;
 }
 
-//Второй способ через ковертацию в строку
+//Второй способ через конвертацию числа в строку
 int SumOfDigits2(int Number)
 {
   String StrNumber = Convert.ToString(Math.Abs(Number));
 
-  int result = 0;
+  int Result = 0;
+  char ZeroChar = '0';
   for (int i = 0; i < StrNumber.Length; i++)
   {
-    result += StrNumber[i] - '0';
+    Result += StrNumber[i] - ZeroChar;
   }
 
-  return result;
+  return Result;
 }
 
 
---
+++ homework_4/task_29/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+using System.Text;
+
+int[] MakeRandomArray(int Size)
+{
+  int[] Result = new int[Size];
+  Random RandomGen = new Random();
+  for (int i = 0; i < Size; i++)
+  {
+    Result[i] = RandomGen.Next();
+  }
+  return Result;
+}
+
+void PrintArray(int[] Array)
+{
+  String separator = ", ";
+  StringBuilder stringBuilder = new StringBuilder("[");
+  for (int i = 0; i < Array.Length - 1; i++)
+  {
+    stringBuilder.Append(Array[i]).Append(separator);
+  }
+  stringBuilder.Append(Array[Array.Length - 1]).Append("]");
+
+  Console.WriteLine(stringBuilder.ToString());
+}
+
+Console.Write("Введите размер массива: ");
+int ArrSize = Convert.ToInt32(Console.ReadLine());
+int[] Arr = MakeRandomArray(ArrSize);
+PrintArray(Arr);
