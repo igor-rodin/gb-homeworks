610d187d933124806074a7331c9e446690fdfdfe
igor-rodin <igreen1974@gmail.com>
2022-03-25 02:36:23 +0300

    Done task 34

--- homework_5/task_34/Program.cs
+++ homework_5/task_34/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34. Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве
+using System.Text;
+
+int[] Create3DigitNumbersArray(int Size)
+{
+  int[] Result = new int[Size];
+
+  Random genInt = new Random();
+  for (int i = 0; i < Size; i++)
+  {
+    Result[i] = genInt.Next(100, 1000);
+  }
+
+  return Result;
+}
+
+void PrintArray(int[] Array)
+{
+  String separator = ", ";
+  StringBuilder stringBuilder = new StringBuilder("[");
+  for (int i = 0; i < Array.Length - 1; i++)
+  {
+    stringBuilder.Append(Array[i]).Append(separator);
+  }
+  stringBuilder.Append(Array[Array.Length - 1]).Append("]");
+
+  Console.WriteLine(stringBuilder.ToString());
+}
+
+int EvensCount(int[] Array)
+{
+  int Result = 0;
+
+  for (int i = 0; i < Array.Length; i++)
+  {
+    if (Array[i] % 2 == 0)
+    {
+      Result++;
+    }
+  }
+
+  return Result;
+}
+
+Console.Write("Введите размер массива: ");
+int ArrSize = Convert.ToInt32(Console.ReadLine());
+int[] Array = Create3DigitNumbersArray(ArrSize);
+PrintArray(Array);
+Console.WriteLine($"Количество четных элементов в массиве: {EvensCount(Array)}");
