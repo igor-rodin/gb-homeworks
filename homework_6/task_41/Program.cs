0949b270bf3f9ae84272355bd2a3885638ea24fb
igor-rodin <igreen1974@gmail.com>
2022-03-28 23:38:17 +0300

    Done task 41

--- homework_6/task_41/Program.cs
+++ homework_6/task_41/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41. Пользователь вводит с клавиатуры или задает самостоятельно M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int CountPositiveInputs(int InputLength)
+{
+  int CountPositive = 0;
+  for (int i = 0; i < InputLength; i++)
+  {
+    int Number = Convert.ToInt32(Console.ReadLine());
+    if (Number > 0)
+    {
+      CountPositive++;
+    }
+  }
+
+  return CountPositive;
+}
+
+Console.WriteLine("Задайте количество чисел, которое хотите ввести: ");
+
+int InputLength = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Теперь вводите");
+
+int CountPositive = CountPositiveInputs(InputLength);
+
+Console.WriteLine($"Положительных чисел: {CountPositive}");
+
+
+
