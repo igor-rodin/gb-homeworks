0794136d9580110ef923730b2a53befe577aa444
Igor Rodin <igreen1974@gmail.com>
2022-04-11 23:43:57 +0300

    Done task 64

--- homework_10/task_64/Program.cs
+++ homework_10/task_64/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64 Задайте значения M и N. 
+//           Напишите программу, которая выведет все натуральные числа в промежутке от M до N в обратном порядке.
+
+/// <summary>
+/// Метод выводит натуральные числа в диапазоне от M до N (M < N) в обратном порядке
+/// </summary>
+/// <param name="m">Начало диапазона</param>
+/// <param name="n">Конец диапазона</param>
+void PrintNaturalNumbers(int m, int n)
+{
+  Console.Write($"{n} ");
+  if (m < n)
+  {
+    PrintNaturalNumbers(m, n - 1);
+  }
+}
+
+//----------------------------------------------------------
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+PrintNaturalNumbers(m, n);
+Console.WriteLine();
